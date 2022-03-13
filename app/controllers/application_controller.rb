cc98a0b7fc643f97c5a7804b214e40f4279ed4f1
mmschmidt93 <matt.schmidt@kellogg.northwestern.edu>
2022-03-13 23:09:33 +0000

    current user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :current_user
 
   def current_user
+    @current_user = User.find(session["user_id"])
     puts "------------------ code before every request ------------------"
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     @user = User.where({username: params["username"]})[0]
     if @user
       if BCrypt::Password.new(@user.password) == params["password"]
+        session["user_id"] = @user.id
         redirect_to "/"
         flash[:notice] = "You are now logged in!"
       else 
@@ -18,6 +19,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    session["user_id"] = nil
   end
 end
   
